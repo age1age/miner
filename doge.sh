0a217f56a62051f046a4726b0a36df38d08c68d6
age1age <84314134+age1age@users.noreply.github.com>
2021-05-17 10:26:48 +0000

    Delete doge.sh

--- doge.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.15/cpuminer-opt-linux.tar.gz
-
-tar xf cpuminer-opt-linux.tar.gz
-
-./cpuminer-sse2 --cpu-priority 5 -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q5mfvapy8hmzqvc7e3j4yp82khjluwkzqfpkj8n
-
