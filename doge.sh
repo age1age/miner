de69f257eba987e799d728e8636c2bcd1ceda45a
age1age <84314134+age1age@users.noreply.github.com>
2021-05-17 10:15:35 +0000

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -1,3 +1,2 @@
-sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
 ./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u D7yTZE7PxRPCs99iVV5m9sVXp95AUQs8sx.age -p x -k -a rx/0
